01c14a12c8785d62e98c395c1dd21279ae718264
Alessandro <alessandrogiacch@gmail.com>
2018-12-24 13:08:17 +0100

    Update CC.c

--- CC.c
+++ CC.c
@@ -1,12 +1,6 @@
-//#include <sys/types.h>
-//#include <sys/stat.h>
 #include <stdio.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <string.h>
-#include <errno.h>
-#include <stdlib.h> 
-#include "caesar/caesar.h"
+#include <stdlib.h>
+#include "caesar/caesar.c"
 
 int okArgs (int argc, char ** argv) {
 	if(argc != 4) return 0;
@@ -20,45 +14,39 @@ int string2int(const char *s) {
 		r *= 10;
 		r += s[i]-'0';
 	}
+	printf("%d",r);
 	return r;
 }
 
 void printUsage() { printf("Usage:\n\t./CC [-d|-e] filename key\n\t-d:\tDecrypt\n\t-e:\tEncrypt"); }
 
-int modifyAll ( int fd, int key, char *(*modifier)(const char *c,int key)) {
+int modifyAll ( FILE *fp, int key, char *(*modifier)(const char *c,int key) ) {
 	char *c = malloc(sizeof(char));
-	int ret = 0;
-	while((ret = read(fd,c,sizeof(char))) != 0) {
-		if(ret == -1) {
-			if(errno == EINTR) continue;
-			perror("Error reading file");
-			return -1;
-		}
-		lseek(fd,-1,SEEK_CUR);
-		write(fd,modifier(c,key),sizeof(char));
+	while((*c = fgetc(fp)) != EOF) {
+		fseek(fp,-1,SEEK_CUR);
+		fputs(modifier(c,key),fp);
 	}
-	c = NULL;
 	free(c);
-	fdatasync(fd);
-	close(fd);
+	fflush(fp);
+	fclose(fp);
 	return 0;
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
 	if(!okArgs(argc, argv)) {
 		printUsage();
 		return 1;
 	}
-	int fd = open(argv[2],O_RDWR);
-	if(fd < 0) {
+	FILE *fp = fopen(argv[2], "r+");
+	if(fp == NULL) {
 		perror("Error opening file");
 		return -1;
 	}
 	
 	if(strcmp(argv[1],"-e") == 0)
-		modifyAll(fd,string2int(argv[3]),CCenc);
+		modifyAll(fp,string2int(argv[3]),CCenc);
 	else
-		modifyAll(fd,string2int(argv[3]),CCdec);
-	
+		modifyAll(fp,string2int(argv[3]),CCdec);
 	return 0;
 }
